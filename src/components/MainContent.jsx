e4626ef7ea846c8dce781af972c5ad5865d0fe17
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-02-23 09:01:10 +0100

    refactor: moved the form and memeContainer into individual components

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,31 +1,11 @@
-import memeImg from '../assets/memeimg.png';
+import Form from './Form';
+import MemeContainer from './MemeContainer';
 
 export default function MainContent() {
   return (
     <main>
-      <form className="form__get-meme">
-        <div className="form--control">
-          <input id="meme-input__top-text" type="text" placeholder="Shut up" />
-        </div>
-
-        <div className="form--control">
-          <input
-            id="meme-input__bottom-text"
-            type="text"
-            placeholder="and take my money"
-          />
-        </div>
-
-        <button id="btn__get--meme" onClick={e => e.preventDefault()}>
-          Get a new meme image 🖼
-        </button>
-      </form>
-
-      <div className="meme--container">
-        <img className="meme--img" src={memeImg} alt="meme" />
-        <h2 className="meme--top-text">shut up</h2>
-        <h2 className="meme--bottom-text">and take my money</h2>
-      </div>
+      <Form />
+      <MemeContainer />
     </main>
   );
 }
